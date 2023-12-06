45233bff19547d9e7c302e36582d461f04d1e5a3
envelope <chaselonas@gmail.com>
2023-12-06 15:56:08 -0800

    added CREATE functionality for basic CRUD operation

--- index.ts
+++ index.ts
@@ -3,10 +3,74 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+interface Item {
+  id: number;
+  name: string;
+}
+
+let item_list: Item[] = [];
+
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+app.get('/greet', (req, res) => {
+
+  const name = req.query.name;
+
+  if (name) {
+    res.send(`Hello ${name}`);
+  } else {
+    res.send('Hello stranger');
+  }
+});
+
+app.post('/submit', (req, res) => {
+    console.log(`Received POST request on: ${port}`);
+
+    const message = req.body.message;
+    res.send(`Message received: ${message}`);
+});
+
+
+
+//// CRUD operation routes
+
+// Create  | (POST)
+app.post('/Crud/createItem', (req, res) => {
+  console.log(`CREATE REQUEST`);
+
+  const newItem: Item = {
+    id: item_list.length + 1,
+    name: req.body.name,
+  };
+
+  item_list.push(newItem);
+
+  console.log(item_list);
+  res.send(`Item created: ${newItem.name}`);
+
+});
+
+// Read    | (GET)
+app.get('/Crud/readItem', (req, res) => {
+  console.log(`READ REQUEST`);
+});
+
+// Update  | (PUT)
+app.put('/Crud/updateItem', (req, res) => {
+  console.log(`UPDATE REQUEST`);
+});
+
+// Delete  | (DELETE)
+app.delete('/Crud/deleteItem', (req, res) => {
+  console.log(`DELETE REQUEST`);
+});
+
+
+app.listen(port, () => { 
   console.log(`Server running on http://localhost:${port}`);
 });
